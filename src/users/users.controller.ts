4c636280618a4a96f1cd1b3fcc9fe3354c2ee064
Leonardo Machado <lmbaudin2013@gmail.com>
2025-11-19 22:46:39 -0300

    Feat/Fix: Fixed schema and implemented rest of user methods and services aswell as material_category sketch

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,48 @@
-import { Controller, Post, Body, ConflictException } from '@nestjs/common';
+import { Controller, Post, Body, ConflictException, Get, Put, Delete } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
+  
+  @Get()
+  async getAllUsers() {
+    const users = await this.usersService.getAllUsers();
+    return users;
+  }
+
+  @Get(':uuid')
+  async getUserById(uuid: string) {
+    const user = await this.usersService.findById(uuid);
+    return user;
+  }
+
+  @Get('username/:username')
+  async getUserByUsername(username: string) {
+    const user = await this.usersService.findOne(username);
+    return user;
+  }
+
+  @Put(':uuid')
+  async updateUser(uuid: string, @Body() body: { username: string; email: string; password: string }) {
+    const { username, email, password } = body;
+
+    const user = await this.usersService.updateUser(uuid, username, email, password);
+
+    return {
+      id: user.uuid,
+      username: user.username,
+      message: 'User updated successfully',
+    };
+  }
+
+  @Delete(':uuid')
+  async deleteUser(uuid: string) {
+    await this.usersService.deleteUser(uuid);
+    return {
+      message: 'User deleted successfully',
+    };
+  }
 
   @Post('register')
   async register(@Body() body: { username: string; email: string; password: string }) {
@@ -17,7 +56,7 @@ export class UsersController {
     const user = await this.usersService.createUser(username, email, password);
 
     return {
-      id: user.id,
+      id: user.uuid,
       username: user.username,
       message: 'User registered successfully',
     };
